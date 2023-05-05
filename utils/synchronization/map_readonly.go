32f28fb457f723d4efa33ca413e441bbf98bc563
kercylan98 <kercylan@gmail.com>
2023-05-05 17:01:20 +0800

    同步map只读接口

---
+++ utils/synchronization/map_readonly.go
@@ -0,0 +1,17 @@
+package synchronization
+
+// MapReadonly 并发安全的只读字典接口
+type MapReadonly[Key comparable, Value any] interface {
+	Get(key Key) Value
+	Exist(key Key) bool
+	GetExist(key Key) (Value, bool)
+	Length() int
+	Range(handle func(key Key, value Value))
+	RangeSkip(handle func(key Key, value Value) bool)
+	RangeBreakout(handle func(key Key, value Value) bool)
+	RangeFree(handle func(key Key, value Value, skip func(), breakout func()))
+	Keys() []Key
+	Slice() []Value
+	Map() map[Key]Value
+	Size() int
+}
