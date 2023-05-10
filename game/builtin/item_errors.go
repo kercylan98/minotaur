eb31ec39827ff781e3b369a1900cefa7db58d2a0
kercylan98 <kercylan@gmail.com>
2023-05-10 10:34:13 +0800

    移除物品概念，不合理

--- game/builtin/item_errors.go
+++
@@ -1,8 +0,0 @@
-package builtin
-
-import "errors"
-
-var (
-	ErrItemInsufficientQuantityDeduction = errors.New("insufficient quantity deduction")
-	ErrItemStackLimit                    = errors.New("the number of items reaches the stacking limit")
-)
