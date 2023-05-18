a2429d5b99784dc31557fa20de7bb0a58020918c
kercylan98 <kercylan@gmail.com>
2023-05-18 19:47:55 +0800

    模板优化

--- planner/configexport/configexport_test.go
+++ planner/configexport/configexport_test.go
@@ -3,7 +3,7 @@ package configexport
 import "testing"
 
 func TestNew(t *testing.T) {
-	c := New(`D:\sources\minotaur\planner\configexport\template.xlsx`)
+	c := New(`./template.xlsx`)
 
 	c.ExportGo("server", "./example")
 	c.ExportClient("client", "./example")
