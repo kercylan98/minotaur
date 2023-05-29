9a9b61320394334f2ec26216753a6f51ca13a077
kercylan98 <kercylan@gmail.com>
2023-05-29 15:44:48 +0800

    随机数范围错误修复

--- utils/random/number.go
+++ utils/random/number.go
@@ -7,7 +7,7 @@ import (
 
 // Int64 返回一个介于min和max之间的int64类型的随机数。
 func Int64(min int64, max int64) int64 {
-	return min + rand.Int63n(max-min)
+	return min + rand.Int63n(max+1-min)
 }
 
 // Int 返回一个介于min和max之间的的int类型的随机数。
