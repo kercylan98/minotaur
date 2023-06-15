3c4eb23041ed7dea1b06c128a0538b74b35cfbcf
kercylan98 <kercylan@gmail.com>
2023-06-15 09:59:16 +0800

    :sparkles: 增加数字取大小辅助函数 Min、Max、MinMax、MaxMin

--- utils/maths/math.go
+++ utils/maths/math.go
@@ -1,5 +1,9 @@
 package maths
 
+import (
+	"github.com/kercylan98/minotaur/utils/generic"
+)
+
 // Pow 整数幂运算
 func Pow(a, n int) int {
 	if a == 0 {
@@ -46,4 +50,36 @@ func PowInt64(a, n int64) int64 {
 		n = n >> 1
 	}
 	return result
+}
+
+// Min 返回两个数之中较小的值
+func Min[V generic.Number](a, b V) V {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// Max 返回两个数之中较大的值
+func Max[V generic.Number](a, b V) V {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// MinMax 将两个数按照较小的和较大的顺序进行返回
+func MinMax[V generic.Number](a, b V) (min, max V) {
+	if a < b {
+		return a, b
+	}
+	return b, a
+}
+
+// MaxMin 将两个数按照较大的和较小的顺序进行返回
+func MaxMin[V generic.Number](a, b V) (max, min V) {
+	if a > b {
+		return a, b
+	}
+	return b, a
 }
