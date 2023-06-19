f64edc7fe8a2d6a3d695622c90fed0ac228891b5
kercylan98 <kercylan@gmail.com>
2023-06-19 10:57:24 +0800

    :art: 优化单元测试及示例

--- utils/geometry/circle_test.go
+++ utils/geometry/circle_test.go
@@ -1,11 +1,1 @@
 package geometry_test
-
-import (
-	"fmt"
-	"github.com/kercylan98/minotaur/utils/geometry"
-	"testing"
-)
-
-func TestGenerateCircle(t *testing.T) {
-	fmt.Println(geometry.GenerateCircle[float64](5, 12))
-}
