d06c840c463810f56b2023751ea15261c5298b85
kercylan98 <kercylan@gmail.com>
2023-07-27 12:06:17 +0800

    feat: generic 包支持更多的空指针判断函数

--- utils/generic/generic.go
+++ utils/generic/generic.go
@@ -6,3 +6,23 @@ import "reflect"
 func IsNil[V any](v V) bool {
 	return reflect.ValueOf(v).IsNil()
 }
+
+// IsAllNil 检查指定的值是否全部为 nil
+func IsAllNil[V any](v ...V) bool {
+	for _, v := range v {
+		if !IsNil(v) {
+			return false
+		}
+	}
+	return true
+}
+
+// IsHasNil 检查指定的值是否存在 nil
+func IsHasNil[V any](v ...V) bool {
+	for _, v := range v {
+		if IsNil(v) {
+			return true
+		}
+	}
+	return false
+}
