89c32b4ce3f187c78e5673a4bdca885e0ca3563d
kercylan98 <kercylan@gmail.com>
2023-08-22 14:07:04 +0800

    feat: super 包新增 StringToFloat64 函数

--- utils/super/parse.go
+++ utils/super/parse.go
@@ -7,3 +7,9 @@ func StringToInt(value string) int {
 	i, _ := strconv.Atoi(value)
 	return i
 }
+
+// StringToFloat64 字符串转换为 float64
+func StringToFloat64(value string) float64 {
+	result, _ := strconv.ParseFloat(value, 64)
+	return result
+}
