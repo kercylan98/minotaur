4982e6d7b691c16b634d6d79c1cf5119eaf89524
kercylan98 <kercylan@gmail.com>
2023-09-11 14:50:27 +0800

    test: slice 包新增部分单元测试

--- utils/slice/drop_test.go
+++ utils/slice/drop_test.go
@@ -1,6 +1,7 @@
 package slice_test
 
 import (
+	"fmt"
 	"github.com/kercylan98/minotaur/utils/slice"
 	"testing"
 )
@@ -8,4 +9,18 @@ import (
 func TestDrop(t *testing.T) {
 	s := []int{1, 2, 3, 4, 5}
 	t.Log(s, slice.Drop(1, 3, s))
+}
+
+func ExampleDrop() {
+	fmt.Println(slice.Drop(1, 3, []int{1, 2, 3, 4, 5}))
+	// Output:
+	// [1 5]
+}
+
+func BenchmarkDrop(b *testing.B) {
+	s := []int{1, 2, 3, 4, 5}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		slice.Drop(1, 3, s)
+	}
 }
