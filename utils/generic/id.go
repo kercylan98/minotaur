5259e07a320c055d15410c96ab35b094d569d19c
kercylan98 <kercylan@gmail.com>
2023-10-16 18:43:10 +0800

    feat: generic 包新增 IDR、IDW、IDRW 的泛型通用接口

--- utils/generic/id.go
+++ utils/generic/id.go
@@ -4,11 +4,24 @@ type IdR[ID comparable] interface {
 	GetId() ID
 }
 
+type IDR[ID comparable] interface {
+	GetID() ID
+}
+
 type IdW[ID comparable] interface {
 	SetId(id ID)
 }
 
-type IdRW[ID comparable] interface {
+type IDW[ID comparable] interface {
+	SetID(id ID)
+}
+
+type IdR2W[ID comparable] interface {
 	IdR[ID]
 	IdW[ID]
+}
+
+type IDR2W[ID comparable] interface {
+	IDR[ID]
+	IDW[ID]
 }
