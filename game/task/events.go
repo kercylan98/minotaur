98c1f39ce6a40abb4097842c545216dc180342a4
kercylan98 <kercylan@gmail.com>
2023-11-09 12:08:27 +0800

    refactor: #60 重构 game/task 包，支持更灵活的任务配置方式

--- game/task/events.go
+++ game/task/events.go
@@ -1,35 +1,77 @@
 package task
 
+import (
+	"reflect"
+)
+
 type (
-	RefreshTaskCountEvent      func(taskType int, increase int64)
-	RefreshTaskChildCountEvent func(taskType int, key any, increase int64)
+	RefreshTaskCounterEventHandler[Trigger any]   func(taskType string, trigger Trigger, count int64)         // 刷新任务计数器事件处理函数
+	RefreshTaskConditionEventHandler[Trigger any] func(taskType string, trigger Trigger, condition Condition) // 刷新任务条件事件处理函数
 )
 
 var (
-	refreshTaskCountEventHandles      = make(map[int]RefreshTaskCountEvent)
-	refreshTaskChildCountEventHandles = make(map[int]RefreshTaskChildCountEvent)
+	refreshTaskCounterEventHandlers = make(map[string][]struct {
+		t reflect.Type
+		h func(taskType string, trigger any, count int64)
+	})
+	refreshTaskConditionEventHandlers = make(map[string][]struct {
+		t reflect.Type
+		h func(taskType string, trigger any, condition Condition)
+	})
 )
 
-// RegRefreshTaskCount 注册任务计数刷新事件
-func RegRefreshTaskCount(taskType int, handler RefreshTaskCountEvent) {
-	refreshTaskCountEventHandles[taskType] = handler
+// RegisterRefreshTaskCounterEvent 注册特定任务类型的刷新任务计数器事件处理函数
+func RegisterRefreshTaskCounterEvent[Trigger any](taskType string, handler RefreshTaskCounterEventHandler[Trigger]) {
+	if refreshTaskCounterEventHandlers == nil {
+		refreshTaskCounterEventHandlers = make(map[string][]struct {
+			t reflect.Type
+			h func(taskType string, trigger any, count int64)
+		})
+	}
+	refreshTaskCounterEventHandlers[taskType] = append(refreshTaskCounterEventHandlers[taskType], struct {
+		t reflect.Type
+		h func(taskType string, trigger any, count int64)
+	}{reflect.TypeOf(handler).In(1), func(taskType string, trigger any, count int64) {
+		handler(taskType, trigger.(Trigger), count)
+	}})
 }
 
-// OnRefreshTaskCount 触发任务计数刷新事件
-func OnRefreshTaskCount(taskType int, increase int64) {
-	if handler, ok := refreshTaskCountEventHandles[taskType]; ok {
-		handler(taskType, increase)
+// OnRefreshTaskCounterEvent 触发特定任务类型的刷新任务计数器事件
+func OnRefreshTaskCounterEvent(taskType string, trigger any, count int64) {
+	if handlers, exist := refreshTaskCounterEventHandlers[taskType]; exist {
+		for _, handler := range handlers {
+			if !reflect.TypeOf(trigger).AssignableTo(handler.t) {
+				continue
+			}
+			handler.h(taskType, trigger, count)
+		}
 	}
 }
 
-// RegRefreshTaskChildCount 注册任务子计数刷新事件
-func RegRefreshTaskChildCount(taskType int, handler RefreshTaskChildCountEvent) {
-	refreshTaskChildCountEventHandles[taskType] = handler
+// RegisterRefreshTaskConditionEvent 注册特定任务类型的刷新任务条件事件处理函数
+func RegisterRefreshTaskConditionEvent[Trigger any](taskType string, handler RefreshTaskConditionEventHandler[Trigger]) {
+	if refreshTaskConditionEventHandlers == nil {
+		refreshTaskConditionEventHandlers = make(map[string][]struct {
+			t reflect.Type
+			h func(taskType string, trigger any, condition Condition)
+		})
+	}
+	refreshTaskConditionEventHandlers[taskType] = append(refreshTaskConditionEventHandlers[taskType], struct {
+		t reflect.Type
+		h func(taskType string, trigger any, condition Condition)
+	}{reflect.TypeOf(handler).In(1), func(taskType string, trigger any, condition Condition) {
+		handler(taskType, trigger.(Trigger), condition)
+	}})
 }
 
-// OnRefreshTaskChildCount 触发任务子计数刷新事件
-func OnRefreshTaskChildCount(taskType int, key any, increase int64) {
-	if handler, ok := refreshTaskChildCountEventHandles[taskType]; ok {
-		handler(taskType, key, increase)
+// OnRefreshTaskConditionEvent 触发特定任务类型的刷新任务条件事件
+func OnRefreshTaskConditionEvent(taskType string, trigger any, condition Condition) {
+	if handlers, exist := refreshTaskConditionEventHandlers[taskType]; exist {
+		for _, handler := range handlers {
+			if !reflect.TypeOf(trigger).AssignableTo(handler.t) {
+				continue
+			}
+			handler.h(taskType, trigger, condition)
+		}
 	}
 }
