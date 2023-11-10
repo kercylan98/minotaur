9157c6a309d8561b9ed701bb8c1d15383f2d371a
kercylan98 <kercylan@gmail.com>
2023-11-10 11:57:43 +0800

    feat: super 包新增 Hostname 函数获取主机名

--- utils/super/super.go
+++ utils/super/super.go
@@ -1,10 +1,18 @@
 package super
 
-import "time"
+import (
+	"os"
+	"time"
+)
 
 var launchTime = time.Now()
 
 // LaunchTime 获取程序启动时间
 func LaunchTime() time.Time {
 	return launchTime
+}
+
+// Hostname 获取主机名
+func Hostname() string {
+	return os.Getenv("HOSTNAME")
 }
