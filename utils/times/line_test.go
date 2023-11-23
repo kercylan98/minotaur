a9c84caa52d690ca86364666a34fbd61a1fd35f8
kercylan98 <kercylan@gmail.com>
2023-11-23 12:03:49 +0800

    feat: times 包新增 Line 时间线结构，提供了时间线性状态的实现

---
+++ utils/times/line_test.go
@@ -0,0 +1,20 @@
+package times_test
+
+import (
+	"github.com/kercylan98/minotaur/utils/times"
+	"testing"
+	"time"
+)
+
+func TestNewStateLine(t *testing.T) {
+	sl := times.NewStateLine(0)
+	sl.AddState(1, time.Now())
+	sl.AddState(2, time.Now().Add(-times.Hour))
+
+	sl.Range(func(index int, state int, ts time.Time) bool {
+		t.Log(index, state, ts)
+		return true
+	})
+
+	t.Log(sl.GetState(time.Now()))
+}
