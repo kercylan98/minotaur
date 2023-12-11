5ffd8163f0ff1c6fb70cf8c3ac6e23383b59b08c
kercylan98 <kercylan@gmail.com>
2023-12-11 11:47:39 +0800

    feat: super 包新增 NumberToRome 函数，支持将整数转为罗马数字

--- utils/super/number.go
+++ utils/super/number.go
@@ -2,8 +2,20 @@ package super
 
 import "reflect"
 
+var (
+	romeThousands = []string{"", "M", "MM", "MMM"}
+	romeHundreds  = []string{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}
+	romeTens      = []string{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}
+	romeOnes      = []string{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}
+)
+
 // IsNumber 判断是否为数字
 func IsNumber(v any) bool {
 	kind := reflect.Indirect(reflect.ValueOf(v)).Kind()
 	return kind >= reflect.Int && kind <= reflect.Float64
+}
+
+// NumberToRome 将数字转换为罗马数字
+func NumberToRome(num int) string {
+	return romeThousands[num/1000] + romeHundreds[num%1000/100] + romeTens[num%100/10] + romeOnes[num%10]
 }
