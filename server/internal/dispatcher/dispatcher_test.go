90b7e4c1f8dad5f60ac60d250a6b3cd1ab03cabf
kercylan98 <kercylan@gmail.com>
2024-01-12 15:52:59 +0800

    test: dispatcher 包完善测试用例

--- server/internal/dispatcher/dispatcher_test.go
+++ server/internal/dispatcher/dispatcher_test.go
@@ -31,6 +31,7 @@ func TestNewDispatcher(t *testing.T) {
 	}
 
 	for _, c := range cases {
+		c := c
 		t.Run(c.name, func(t *testing.T) {
 			defer func() {
 				if r := recover(); r != nil && !c.shouldPanic {
@@ -54,23 +55,24 @@ func TestDispatcher_SetProducerDoneHandler(t *testing.T) {
 	}
 
 	for _, c := range cases {
+		c := c
 		t.Run(c.name, func(t *testing.T) {
 			c.messageFinish = &atomic.Bool{}
 			w := new(sync.WaitGroup)
 			d := dispatcher.NewDispatcher(1024, c.name, func(dispatcher *dispatcher.Dispatcher[string, *TestMessage], message *TestMessage) {
 				w.Done()
 			})
-			d.SetProducerDoneHandler("producer", func(p string, dispatcher *dispatcher.Dispatcher[string, *TestMessage]) {
+			d.Put(&TestMessage{producer: c.producer})
+			d.SetProducerDoneHandler(c.producer, func(p string, dispatcher *dispatcher.Action[string, *TestMessage]) {
 				c.messageFinish.Store(true)
 			})
 			if c.cancel {
-				d.SetProducerDoneHandler("producer", nil)
+				d.SetProducerDoneHandler(c.producer, nil)
 			}
-			d.Put(&TestMessage{producer: "producer"})
 			w.Add(1)
 			d.Start()
 			w.Wait()
-			if c.messageFinish.Load() && c.cancel {
+			if c.cancel && c.messageFinish.Load() {
 				t.Errorf("%s should cancel, but not", c.name)
 			}
 		})
@@ -90,6 +92,7 @@ func TestDispatcher_SetClosedHandler(t *testing.T) {
 	}
 
 	for _, c := range cases {
+		c := c
 		t.Run(c.name, func(t *testing.T) {
 			c.handlerFinishMsgCount = &atomic.Int64{}
 			w := new(sync.WaitGroup)
@@ -97,7 +100,7 @@ func TestDispatcher_SetClosedHandler(t *testing.T) {
 				time.Sleep(c.msgTime)
 				c.handlerFinishMsgCount.Add(1)
 			})
-			d.SetClosedHandler(func(dispatcher *dispatcher.Dispatcher[string, *TestMessage]) {
+			d.SetClosedHandler(func(dispatcher *dispatcher.Action[string, *TestMessage]) {
 				w.Done()
 			})
 			for i := 0; i < c.msgCount; i++ {
@@ -109,6 +112,41 @@ func TestDispatcher_SetClosedHandler(t *testing.T) {
 			w.Wait()
 			if c.handlerFinishMsgCount.Load() != int64(c.msgCount) {
 				t.Errorf("%s should finish %d messages, but finish %d", c.name, c.msgCount, c.handlerFinishMsgCount.Load())
+			}
+		})
+	}
+}
+
+func TestIncrCount(t *testing.T) {
+	var cases = []struct {
+		name        string
+		producer    string
+		messageDone *atomic.Bool
+	}{
+		{name: "TestIncrCount_Normal", producer: "producer"},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			c.messageDone = &atomic.Bool{}
+			w := new(sync.WaitGroup)
+			w.Add(1)
+			var d *dispatcher.Dispatcher[string, *TestMessage]
+			d = dispatcher.NewDispatcher(1024, c.name, func(dispatcher *dispatcher.Dispatcher[string, *TestMessage], message *TestMessage) {
+				c.messageDone.Store(true)
+				d.IncrCount(c.producer, -1)
+			})
+			d.SetClosedHandler(func(dispatcher *dispatcher.Action[string, *TestMessage]) {
+				w.Done()
+			})
+			d.Start()
+			d.IncrCount(c.producer, 1)
+			d.Expel()
+			d.Put(&TestMessage{producer: c.producer})
+			w.Wait()
+			if !c.messageDone.Load() {
+				t.Errorf("%s should done, but not", c.name)
 			}
 		})
 	}
@@ -127,6 +165,7 @@ func TestDispatcher_Expel(t *testing.T) {
 	}
 
 	for _, c := range cases {
+		c := c
 		t.Run(c.name, func(t *testing.T) {
 			c.handlerFinishMsgCount = &atomic.Int64{}
 			w := new(sync.WaitGroup)
@@ -134,7 +173,7 @@ func TestDispatcher_Expel(t *testing.T) {
 				time.Sleep(c.msgTime)
 				c.handlerFinishMsgCount.Add(1)
 			})
-			d.SetClosedHandler(func(dispatcher *dispatcher.Dispatcher[string, *TestMessage]) {
+			d.SetClosedHandler(func(dispatcher *dispatcher.Action[string, *TestMessage]) {
 				w.Done()
 			})
 			for i := 0; i < c.msgCount; i++ {
@@ -163,13 +202,14 @@ func TestDispatcher_UnExpel(t *testing.T) {
 	}
 
 	for _, c := range cases {
+		c := c
 		t.Run(c.name, func(t *testing.T) {
 			c.closed = &atomic.Bool{}
 			w := new(sync.WaitGroup)
 			d := dispatcher.NewDispatcher(1024, c.name, func(dispatcher *dispatcher.Dispatcher[string, *TestMessage], message *TestMessage) {
 				w.Done()
 			})
-			d.SetClosedHandler(func(dispatcher *dispatcher.Dispatcher[string, *TestMessage]) {
+			d.SetClosedHandler(func(dispatcher *dispatcher.Action[string, *TestMessage]) {
 				c.closed.Store(true)
 			})
 			d.Put(&TestMessage{producer: "producer"})
@@ -184,6 +224,108 @@ func TestDispatcher_UnExpel(t *testing.T) {
 			w.Wait()
 			if c.closed.Load() != c.expect {
 				t.Errorf("%s should %v, but %v", c.name, c.expect, c.closed.Load())
+			}
+		})
+	}
+}
+
+func TestDispatcher_Put(t *testing.T) {
+	var cases = []struct {
+		name        string
+		producer    string
+		messageDone *atomic.Bool
+	}{
+		{name: "TestDispatcher_Put_Normal", producer: "producer"},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			c.messageDone = &atomic.Bool{}
+			w := new(sync.WaitGroup)
+			w.Add(1)
+			d := dispatcher.NewDispatcher(1024, c.name, func(dispatcher *dispatcher.Dispatcher[string, *TestMessage], message *TestMessage) {
+				c.messageDone.Store(true)
+				w.Done()
+			})
+			d.Start()
+			d.Put(&TestMessage{producer: c.producer})
+			d.Expel()
+			w.Wait()
+			if !c.messageDone.Load() {
+				t.Errorf("%s should done, but not", c.name)
+			}
+		})
+	}
+}
+
+func TestDispatcher_Start(t *testing.T) {
+	var cases = []struct {
+		name        string
+		producer    string
+		messageDone *atomic.Bool
+	}{
+		{name: "TestDispatcher_Start_Normal", producer: "producer"},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			c.messageDone = &atomic.Bool{}
+			w := new(sync.WaitGroup)
+			w.Add(1)
+			d := dispatcher.NewDispatcher(1024, c.name, func(dispatcher *dispatcher.Dispatcher[string, *TestMessage], message *TestMessage) {
+				c.messageDone.Store(true)
+				w.Done()
+			})
+			d.Start()
+			d.Put(&TestMessage{producer: c.producer})
+			d.Expel()
+			w.Wait()
+			if !c.messageDone.Load() {
+				t.Errorf("%s should done, but not", c.name)
+			}
+		})
+	}
+}
+
+func TestDispatcher_Name(t *testing.T) {
+	var cases = []struct {
+		name string
+	}{
+		{name: "TestDispatcher_Name_Normal"},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			d := dispatcher.NewDispatcher(1024, c.name, func(dispatcher *dispatcher.Dispatcher[string, *TestMessage], message *TestMessage) {})
+			if d.Name() != c.name {
+				t.Errorf("%s should equal %s, but not", c.name, c.name)
+			}
+		})
+	}
+}
+
+func TestDispatcher_Closed(t *testing.T) {
+	var cases = []struct {
+		name string
+	}{
+		{name: "TestDispatcher_Closed_Normal"},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			w := new(sync.WaitGroup)
+			w.Add(1)
+			d := dispatcher.NewDispatcher(1024, c.name, func(dispatcher *dispatcher.Dispatcher[string, *TestMessage], message *TestMessage) {})
+			d.SetClosedHandler(func(dispatcher *dispatcher.Action[string, *TestMessage]) { w.Done() })
+			d.Start()
+			d.Expel()
+			w.Wait()
+			if !d.Closed() {
+				t.Errorf("%s should closed, but not", c.name)
 			}
 		})
 	}
