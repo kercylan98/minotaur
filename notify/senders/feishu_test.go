2b464021b010b7c7a21c056f987de8a5d76c65ee
kercylan98 <kercylan@gmail.com>
2024-01-15 11:49:21 +0800

    recessive: 其他调整

--- notify/senders/feishu_test.go
+++ notify/senders/feishu_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestFeiShu_Push(t *testing.T) {
-	fs := NewFeiShu("https://open.feishu.cn/open-apis/bot/v2/hook/d886f30f-814c-47b1-aeb0-b508da0f7f22")
+	fs := NewFeiShu("https://open.feishu.cn/open-apis/bot/v2/hook/bid")
 
 	rt := notifies.NewFeiShu(notifies.FeiShuMessageWithRichText(notifies.NewFeiShuRichText().Create("zh_cn", "标题咯").AddText("哈哈哈").Ok()))
 	if err := fs.Push(rt); err != nil {
