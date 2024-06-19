3bf812173f63119647c0f98411fa2fa4b6af856c
kercylan98 <kercylan@gmail.com>
2024-06-19 13:54:25 +0800

    🔥 移除本地 go doc 脚本

--- local-doc.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo Open: http://localhost:9998/pkg/github.com/kercylan98/minotaur/
-godoc -http=:9998 -play
--- local-doc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo Open: http://localhost:9998/pkg/github.com/kercylan98/minotaur/
-godoc -http=:9998 -play
