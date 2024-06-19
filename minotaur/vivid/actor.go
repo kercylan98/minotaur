e560dc827785e72d3d08a71471aa42fceea2d800
kercylan98 <kercylan@gmail.com>
2024-06-19 19:47:01 +0800

    ⚰️ 移除不合理的 FreeActor 设计

--- minotaur/vivid/actor.go
+++ minotaur/vivid/actor.go
@@ -6,18 +6,6 @@ type Actor interface {
 	OnReceive(ctx MessageContext)
 }
 
-type FreeActor[T any] struct {
-	actor T
-}
-
-func (i *FreeActor[T]) OnReceive(ctx MessageContext) {
-
-}
-
-func (i *FreeActor[T]) GetActor() T {
-	return i.actor
-}
-
 func onReceive(actor Actor, ctx MessageContext) {
 	var core *_ActorCore
 	var ok bool
