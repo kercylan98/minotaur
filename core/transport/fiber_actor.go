a89eeb934bc4d089e46628a36b4f3906650adfd9
kercylan98 <kercylan@gmail.com>
2024-07-06 12:18:18 +0800

    🚑️ 修复 FiberActor 中连接打开与关闭的钩子在发生 panic 时会导致 FiberActor 死亡，从而导致所有连接断开的问题。

--- core/transport/fiber_actor.go
+++ core/transport/fiber_actor.go
@@ -100,6 +100,16 @@ func (f *fiberActor) onConnectionOpened(ctx vivid.ActorContext, m *fiberConnActo
 		}, 0))
 	})
 	conn := NewConn(m.fiberConn, ctx.System(), ref, ref)
+	defer func() {
+		if err := recover(); err != nil {
+			switch err.(type) {
+			case error:
+				ctx.Tell(ref, err)
+			default:
+				ctx.Tell(ref, fmt.Errorf("connection opened panic: %v", err))
+			}
+		}
+	}()
 	if err := f.kit.fws.connectionOpenedHook(f.kit, m.ctx, conn); err != nil {
 		ctx.Tell(ref, err)
 	}
@@ -107,6 +117,17 @@ func (f *fiberActor) onConnectionOpened(ctx vivid.ActorContext, m *fiberConnActo
 }
 
 func (f *fiberActor) onConnectionClosed(ctx vivid.ActorContext, m *fiberConnActor) {
+	defer func() {
+		if err := recover(); err != nil {
+			switch err.(type) {
+			case error:
+				ctx.Tell(m.ref, err)
+			default:
+				ctx.Tell(m.ref, fmt.Errorf("connection opened panic: %v", err))
+			}
+		}
+	}()
+
 	f.kit.fws.connectionClosedHook(f.kit, m.ctx, NewConn(m.fiberConn, ctx.System(), m.ref, m.ref), m.err)
 	if m.status.Load() == fiberConnStatusOnline {
 		ctx.TerminateGracefully(m.ref)
--- core/transport/fiber_websocket.go
+++ core/transport/fiber_websocket.go
@@ -25,21 +25,28 @@ func (fw *FiberWebSocket) init() {
 	fw.connectionPacketHook = func(kit *FiberKit, ctx *FiberContext, conn *Conn, packet Packet) error { return nil }
 }
 
+// UpgradeHook 绑定升级钩子函数，该函数将在 WebSocket 升级时调用，返回错误则终止升级
 func (fw *FiberWebSocket) UpgradeHook(hook FiberWebSocketUpgradeHook) *FiberWebSocket {
 	fw.upgradeHook = hook
 	return fw
 }
 
+// ConnectionOpenedHook 绑定连接打开钩子函数，该函数将在连接打开时调用，返回错误则关闭连接
+//   - 该函数的执行会在独立的 FiberActor 中执行，所以是并发安全的
 func (fw *FiberWebSocket) ConnectionOpenedHook(hook ConnectionOpenedHook) *FiberWebSocket {
 	fw.connectionOpenedHook = hook
 	return fw
 }
 
+// ConnectionClosedHook 绑定连接关闭钩子函数，该函数将在连接关闭时调用
+//   - 该函数的执行会在独立的 FiberActor 中执行，所以是并发安全的
 func (fw *FiberWebSocket) ConnectionClosedHook(hook ConnectionClosedHook) *FiberWebSocket {
 	fw.connectionClosedHook = hook
 	return fw
 }
 
+// ConnectionPacketHook 绑定连接数据包钩子函数，该函数将在接收到数据包时调用，返回错误则关闭连接
+//   - 该函数的执行是在维护连接的 Actor 中进行的，连接与连接之间是相互隔离的
 func (fw *FiberWebSocket) ConnectionPacketHook(hook ConnectionPacketHook) *FiberWebSocket {
 	fw.connectionPacketHook = hook
 	return fw
