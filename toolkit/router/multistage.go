7854ff603302983954b94a90dab1aa8a067c823d
kercylan98 <kercylan@gmail.com>
2024-07-10 17:59:42 +0800

    ✨ router.NewMultistage 函数增加类型校验

--- toolkit/router/multistage.go
+++ toolkit/router/multistage.go
@@ -8,6 +8,9 @@ import (
 
 // NewMultistage 创建一个支持多级分类的路由器
 func NewMultistage[HandleFunc any](options ...MultistageOption[HandleFunc]) *Multistage[HandleFunc] {
+	if reflect.TypeOf((*HandleFunc)(nil)).Elem().Kind() != reflect.Func {
+		panic(fmt.Errorf("handle must be a function type"))
+	}
 	r := &Multistage[HandleFunc]{
 		routes: make(map[any]HandleFunc),
 		subs:   make(map[any]*Multistage[HandleFunc]),
@@ -36,9 +39,9 @@ type Multistage[HandleFunc any] struct {
 
 // Register 注册路由是结合 Sub 和 Route 的快捷方式，用于一次性注册多级路由
 //   - 该函数将返回一个注册函数，可通过调用其将路由绑定到特定处理函数，例如：router.Register("a", "b").Bind(onExec())
-func (slf *Multistage[HandleFunc]) Register(routes ...any) MultistageBind[HandleFunc] {
+func (r *Multistage[HandleFunc]) Register(routes ...any) MultistageBind[HandleFunc] {
 	return func(handleFunc HandleFunc) {
-		router := slf
+		router := r
 		for i, route := range routes {
 			if i == len(routes)-1 {
 				router.Route(route, handleFunc)
@@ -50,36 +53,36 @@ func (slf *Multistage[HandleFunc]) Register(routes ...any) MultistageBind[Handle
 }
 
 // Route 为特定路由绑定处理函数，被绑定的处理函数将可以通过 Match 函数进行匹配
-func (slf *Multistage[HandleFunc]) Route(route any, handleFunc HandleFunc) {
+func (r *Multistage[HandleFunc]) Route(route any, handleFunc HandleFunc) {
 	trim := route
-	if slf.trim != nil {
-		trim = slf.trim(route)
+	if r.trim != nil {
+		trim = r.trim(route)
 	}
 	if reflect.TypeOf(handleFunc).Kind() != reflect.Func {
 		panic(fmt.Errorf("route[%v] registration failed, handle must be a function type", route))
 	}
 
-	_, exist := slf.routes[trim]
+	_, exist := r.routes[trim]
 	if exist {
 		panic(fmt.Errorf("the route[%v] has already been registered, duplicate registration is not allowed", route))
 	}
 
-	_, exist = slf.subs[trim]
+	_, exist = r.subs[trim]
 	if exist {
 		panic(fmt.Errorf("the route[%v] has already been registered, duplicate registration is not allowed", route))
 	}
 
-	slf.routes[trim] = handleFunc
-	if slf.tag == nil {
+	r.routes[trim] = handleFunc
+	if r.tag == nil {
 		log.Info("Router", log.String("Type", "Multistage"), log.String("Route", fmt.Sprintf("%v", route)))
 	} else {
-		log.Info("Router", log.String("Type", "Multistage"), log.String("Route", fmt.Sprintf("%v > %v", slf.tag, route)))
+		log.Info("Router", log.String("Type", "Multistage"), log.String("Route", fmt.Sprintf("%v > %v", r.tag, route)))
 	}
 }
 
 // Match 匹配已绑定处理函数的路由，返回处理函数
 //   - 如果未找到将会返回空指针
-func (slf *Multistage[HandleFunc]) Match(routes ...any) HandleFunc {
+func (r *Multistage[HandleFunc]) Match(routes ...any) HandleFunc {
 	if len(routes) == 0 {
 		panic(fmt.Errorf("the route cannot be empty"))
 	}
@@ -87,11 +90,11 @@ func (slf *Multistage[HandleFunc]) Match(routes ...any) HandleFunc {
 	var exist bool
 	var router *Multistage[HandleFunc]
 	for i, route := range routes {
-		handleFunc, exist = slf.routes[route]
+		handleFunc, exist = r.routes[route]
 		if exist {
 			return handleFunc
 		}
-		router, exist = slf.subs[route]
+		router, exist = r.subs[route]
 		if !exist {
 			return handleFunc
 		}
@@ -104,25 +107,25 @@ func (slf *Multistage[HandleFunc]) Match(routes ...any) HandleFunc {
 }
 
 // Sub 获取子路由器
-func (slf *Multistage[HandleFunc]) Sub(route any) *Multistage[HandleFunc] {
+func (r *Multistage[HandleFunc]) Sub(route any) *Multistage[HandleFunc] {
 	trim := route
-	if slf.trim != nil {
-		trim = slf.trim(route)
+	if r.trim != nil {
+		trim = r.trim(route)
 	}
-	_, exist := slf.routes[trim]
+	_, exist := r.routes[trim]
 	if exist {
 		panic(fmt.Errorf("the route[%v] has already been registered, cannot be used as a sub-router", route))
 	}
 
-	router, exist := slf.subs[trim]
+	router, exist := r.subs[trim]
 	if !exist {
 		router = NewMultistage[HandleFunc]()
-		if slf.tag == nil {
+		if r.tag == nil {
 			router.tag = fmt.Sprintf("%v", route)
 		} else {
-			router.tag = fmt.Sprintf("%v > %v", slf.tag, route)
+			router.tag = fmt.Sprintf("%v > %v", r.tag, route)
 		}
-		slf.subs[trim] = router
+		r.subs[trim] = router
 	}
 	return router
 }
