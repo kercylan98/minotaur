d937fc17c945d00c4510839cf80b1fcc6a4cef9e
kercylan98 <kercylan@gmail.com>
2024-07-16 12:03:53 +0800

    ✅ 新增基于 WebSocket 的 StreamClient 使用示例

---
+++ examples/internal/stream-client/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fasthttp/websocket"
+	"github.com/kercylan98/minotaur/core/transport"
+	"github.com/kercylan98/minotaur/core/vivid"
+	"os"
+)
+
+func main() {
+	system := vivid.NewActorSystem()
+	system.ActorOf(func() vivid.Actor {
+		return transport.NewStreamClient(&transport.StreamClientWebsocketCore{
+			Url: "ws://localhost:8877/ws",
+		}, transport.StreamClientConfig{
+			ConnectionOpenedHandler: func(ctx vivid.ActorContext) {
+				fmt.Println("connection opened")
+				ctx.Tell(ctx.Ref(), transport.NewPacket([]byte("hello")).SetContext(websocket.BinaryMessage))
+			},
+			ConnectionPacketHandler: func(ctx vivid.ActorContext, packet transport.Packet) {
+				fmt.Println("packet received", string(packet.GetBytes()))
+			},
+			ConnectionClosedHandler: func(ctx vivid.ActorContext, err error) {
+				fmt.Println("connection closed", err)
+			},
+		})
+	})
+
+	system.Signal(func(system *vivid.ActorSystem, signal os.Signal) {
+		system.ShutdownGracefully()
+	})
+}
