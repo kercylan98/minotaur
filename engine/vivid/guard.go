897d7675acb2b7b574072b4a3374f47db57aebfb
kercylan98 <kercylan@gmail.com>
2024-07-24 13:08:20 +0800

    ✨ vivid 中目前对于未被监管的事故将记录一条 warn 日志

--- engine/vivid/guard.go
+++ engine/vivid/guard.go
@@ -2,6 +2,7 @@ package vivid
 
 import (
 	"github.com/kercylan98/minotaur/engine/vivid/supervision"
+	"github.com/kercylan98/minotaur/toolkit/log"
 	"time"
 )
 
@@ -13,6 +14,7 @@ func (g *guard) OnReceive(ctx ActorContext) {
 	switch ctx.Message().(type) {
 	case *OnLaunch:
 		g.Strategy = supervision.OneForOne(10, time.Millisecond*200, time.Second*3, supervision.FunctionalDecide(func(record *supervision.AccidentRecord) supervision.Directive {
+			ctx.System().Logger().Warn("ActorSystem", log.String("info", "unsupervised accidents"), log.String("actor", record.Victim.LogicalAddress()), log.Any("reason", record.Reason))
 			return supervision.DirectiveRestart
 		}))
 	}
