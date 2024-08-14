53f17b5569b5afdf4d83fce7aaa1cd2f0decae07
kercylan98 <kercylan@gmail.com>
2024-08-14 17:25:09 +0800

    🐛 调整 ax 依赖版本，修复 charproc 包中对于字符串转蛇形一个单词会存在下划线前缀的问题

--- toolkit/charproc/cast.go
+++ toolkit/charproc/cast.go
@@ -1,6 +1,11 @@
 package charproc
 
-import "unicode"
+import (
+	"strings"
+	"unicode"
+)
+
+const vowels = "aeiouAEIOU"
 
 // FirstUpper 返回字符串的首字母大写形式
 func FirstUpper(s string) string {
@@ -29,17 +34,18 @@ func LenWithChinese(s string) int {
 
 // Snake 蛇形字符串
 func Snake(str string) string {
-	var snakeStr string
+	var builder strings.Builder
 	vv := []rune(str)
 	for i := 0; i < len(vv); i++ {
 		if vv[i] >= 65 && vv[i] <= 90 {
 			vv[i] += 32
-			snakeStr += "_" + string(vv[i])
-		} else {
-			snakeStr += string(vv[i])
+			if builder.Len() > 0 {
+				builder.WriteString("_")
+			}
 		}
+		builder.WriteRune(vv[i])
 	}
-	return snakeStr
+	return builder.String()
 }
 
 // BigCamel 大驼峰字符串
@@ -49,20 +55,36 @@ func BigCamel(str string) string {
 
 // Camel 驼峰字符串
 func Camel(str string) string {
-	var camelStr string
+	var builder strings.Builder
 	vv := []rune(str)
 	for i := 0; i < len(vv); i++ {
 		if vv[i] == '_' {
 			i++
 			if vv[i] >= 97 && vv[i] <= 122 {
 				vv[i] -= 32
-				camelStr += string(vv[i])
+				builder.WriteRune(vv[i])
 			} else {
 				return str
 			}
 		} else {
-			camelStr += string(vv[i])
+			builder.WriteRune(vv[i])
 		}
 	}
-	return camelStr
+	return builder.String()
+}
+
+// Plural 将最后一个单词转换为复数形式
+func Plural(word string) string {
+	if strings.HasSuffix(word, "y") && len(word) > 1 && !IsVowel(rune(word[len(word)-2])) {
+		return word[:len(word)-1] + "ies"
+	}
+	if strings.HasSuffix(word, "s") || strings.HasSuffix(word, "x") || strings.HasSuffix(word, "z") || strings.HasSuffix(word, "ch") || strings.HasSuffix(word, "sh") {
+		return word + "es"
+	}
+	return word + "s"
+}
+
+// IsVowel 判断是否为元音
+func IsVowel(ch rune) bool {
+	return strings.ContainsRune(vowels, ch)
 }
